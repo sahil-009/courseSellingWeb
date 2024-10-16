d33107a5bbff0ed5b0dd6ed6eaab3dd018bc8c6b
Sahil Mund <130903382+sahil-009@users.noreply.github.com>
2024-10-16 12:32:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const express =require('express');
+const { createUserRoutes } = require('./routes/user');
+const { createCourseRoutes } = require('./routes/course');
+const app = express();
+
+createUserRoutes(app);
+createCourseRoutes(app);
+
+
+
+
+app.listen(3000);
+
+
